350e712c43e0ffd208dcd7f21756f4f732815062
Brassalsa <sauravchauhan2k@gmail.com>
2024-08-03 20:19:54 +0530

    add env

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,16 @@
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = new Hono();
 
 app.get("/", (c) => {
   return c.text("Hello Hono!");
 });
 
-const port = 3000;
+const port = Number(process.env.PORT) || 3000;
+
 console.log(`Server is running on http://localhost:${port}`);
 
 serve({
