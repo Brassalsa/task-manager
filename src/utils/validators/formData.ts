8c8c6feee236e1140c5fa53c3fc90d6ea6df0352
Brassalsa <sauravchauhan2k@gmail.com>
2024-08-04 19:46:27 +0530

    add task schema

--- src/utils/validators/formData.ts
+++ src/utils/validators/formData.ts
@@ -1,5 +1,8 @@
+import { title } from "process";
 import { z } from "zod";
+import { TaskPriority, TaskStatus } from "../constants";
 
+// user schema
 const password = z
   .string()
   .min(8, { message: "password must be at least 8 characters long" })
@@ -17,4 +20,33 @@ export const registerSchema = z.object({
 export const loginSchema = z.object({
   email: z.string().email({ message: "invalid email address" }),
   password,
+});
+
+// task schema
+const taskSchema = {
+  title: z.string(),
+  description: z.string().optional(),
+  status: z
+    .string()
+    .optional()
+    .default(TaskStatus.pending)
+    .refine((val) => {
+      if (val === undefined) return true;
+      return Object.keys(TaskStatus).includes(val);
+    }),
+  priority: z
+    .string()
+    .optional()
+    .default(TaskPriority.normal)
+    .refine((val) => {
+      if (val === undefined) return true;
+      return Object.keys(TaskPriority).includes(val);
+    }),
+  due_date: z.coerce.date(),
+};
+export const createTaskSchema = z.object(taskSchema);
+
+export const updateTaskSchema = z.object({
+  id: z.coerce.number(),
+  ...taskSchema,
 });
