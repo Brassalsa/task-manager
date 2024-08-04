cde05dbad6da7ec8e04249154315d2a8d5f0d4ce
Brassalsa <sauravchauhan2k@gmail.com>
2024-08-04 16:26:52 +0530

    add tokens and hashing

---
+++ src/utils/auth/jwt.ts
@@ -0,0 +1,13 @@
+import { sign, verify } from "hono/jwt";
+import { getEnvOrThrow } from "../helpers";
+import { JWTPayload } from "hono/utils/jwt/types";
+
+const secret = getEnvOrThrow(process.env.JWT_SECRET, "jwt secret not in env");
+
+export const encodeData = async (payload: JWTPayload) => {
+  return await sign(payload, secret);
+};
+
+export const decodeToken = async (token: string) => {
+  return await verify(token, secret);
+};
---
+++ src/utils/hashing.ts
@@ -0,0 +1,11 @@
+import bcrypt from "bcrypt";
+
+const encKey = 10;
+
+export const hashStr = (str: string) => {
+  return bcrypt.hashSync(str, encKey);
+};
+
+export const compareHash = (str: string, hash: string) => {
+  return bcrypt.compareSync(str, hash);
+};
